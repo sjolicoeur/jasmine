7388ef79e0b8d6cd007d8bd9796711a76e95db5a
Steve Gravrock <sgravrock@pivotal.io>
2017-11-09 19:11:15 -0800

    Refactoring

--- src/core/StackTrace.js
+++ src/core/StackTrace.js
@@ -4,63 +4,48 @@ getJasmineRequireObj().StackTrace = function(j$) {
     this.frames = extractFrames(lines);
   }
 
-  // PhantomJS on Linux, Chrome, Node, IE, Edge
-  function extractChromeStyleFrames(lines) {
-    var converted = lines.map(function(line) {
-      // e.g. "   at QueueRunner.run (http://localhost:8888/__jasmine__/jasmine.js:4320:20)"
-      // 
-      var match = line.match(/^\s*at ([^\)]+) \(([^\)]+)\)$/);
-
-      if (!match) {
-        return null;
-      }
-
-      return makeFrame(match[2], match[1]);
-    });
-
-    // The first line is the top stack frame on PhantomJS
-    // and the error message otherwise.
-    if (lines[0].match(/^Error/)) {
-      converted.shift();
-    }
-
-    return converted[0] ? converted : null;
-  }
-
-  // PhantomJS on OS X, Safari, Firefox
-  function extractFirefoxStyleFrames(lines) {
-    var converted = lines.map(function(line) {
-      // e.g. "run@http://localhost:8888/__jasmine__/jasmine.js:4320:27"
-      // or "http://localhost:8888/__jasmine__/jasmine.js:4320:27"
-      var match = line.match(/^(([^@\s]+)@)?([^\s]+)$/);
-      var result;
-
-      if (!match) {
-        return null;
-      }
-
-      return makeFrame(match[3], match[2]);
-    });
-
-    return converted[0] ? converted : null;
-  }
-
   function extractFrames(lines) {
     return extractChromeStyleFrames(lines) ||
       extractFirefoxStyleFrames(lines) ||
       null;
   }
 
-  function makeFrame(fileAndLineAndCol, func) {
-    var match = fileAndLineAndCol.match(/^(.*):(\d+):\d+$/);
-
-    if (match) {
-      return {
-        file: match[1],
-        line: parseInt(match[2], 10),
-        func: func
-      };
+  // PhantomJS on Linux, Chrome, Node, IE, Edge
+  function extractChromeStyleFrames(lines) {
+    // The first line is the top stack frame on PhantomJS
+    // and the error message otherwise.
+    if (lines[0].match(/^Error/)) {
+      lines = lines.slice(1);
     }
+
+    // e.g. "   at QueueRunner.run (http://localhost:8888/__jasmine__/jasmine.js:4320:20)"
+    return tryExtractFrames(lines, /^\s*at ([^\)]+) \(([^\)]+)\)$/, 1, 2);
+  }
+
+  // PhantomJS on OS X, Safari, Firefox
+  function extractFirefoxStyleFrames(lines) {
+    // e.g. "run@http://localhost:8888/__jasmine__/jasmine.js:4320:27"
+    // or "http://localhost:8888/__jasmine__/jasmine.js:4320:27"
+    return tryExtractFrames(lines, /^(([^@\s]+)@)?([^\s]+)$/, 2, 3);
+  }
+
+  function tryExtractFrames(lines, regex, funcGroupIx, fileAndLineAndColGroupIx) {
+    var converted = lines.map(function(line) {
+      var overallMatch = line.match(regex);
+      var fileLineColMatch;
+      if (!overallMatch) { return null; }
+      fileLineColMatch = overallMatch[fileAndLineAndColGroupIx].match(
+        /^(.*):(\d+):\d+$/);
+      if (!fileLineColMatch) { return null; }
+
+      return {
+        file: fileLineColMatch[1],
+        line: parseInt(fileLineColMatch[2], 10),
+        func: overallMatch[funcGroupIx]
+      };
+    });
+
+    return converted[0] ? converted : null;
   }
 
   return StackTrace;
