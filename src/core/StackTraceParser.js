bb5d4b3134aa37c9f2dddafead8d8e4281c6db40
Steve Gravrock <sgravrock@pivotal.io>
2017-11-09 19:17:35 -0800

    More renaming

--- src/core/StackTraceParser.js
+++ src/core/StackTraceParser.js
@@ -2,36 +2,36 @@ getJasmineRequireObj().StackTraceParser = function(j$) {
   function StackTraceParser() {
     this.parse = function(rawTrace) {
       var lines = rawTrace.split('\n');
-      return extractFrames(lines);
+      return parseFrames(lines);
     };
   }
 
-  function extractFrames(lines) {
-    return extractChromeStyleFrames(lines) ||
-      extractFirefoxStyleFrames(lines) ||
+  function parseFrames(lines) {
+    return parseChromeStyleFrames(lines) ||
+      parseFirefoxStyleFrames(lines) ||
       null;
   }
 
   // PhantomJS on Linux, Chrome, Node, IE, Edge
-  function extractChromeStyleFrames(lines) {
+  // e.g. "   at QueueRunner.run (http://localhost:8888/__jasmine__/jasmine.js:4320:20)"
+  function parseChromeStyleFrames(lines) {
     // The first line is the top stack frame on PhantomJS
     // and the error message otherwise.
     if (lines[0].match(/^Error/)) {
       lines = lines.slice(1);
     }
 
-    // e.g. "   at QueueRunner.run (http://localhost:8888/__jasmine__/jasmine.js:4320:20)"
-    return tryExtractFrames(lines, /^\s*at ([^\)]+) \(([^\)]+)\)$/, 1, 2);
+    return tryParseFrames(lines, /^\s*at ([^\)]+) \(([^\)]+)\)$/, 1, 2);
   }
 
   // PhantomJS on OS X, Safari, Firefox
-  function extractFirefoxStyleFrames(lines) {
-    // e.g. "run@http://localhost:8888/__jasmine__/jasmine.js:4320:27"
-    // or "http://localhost:8888/__jasmine__/jasmine.js:4320:27"
-    return tryExtractFrames(lines, /^(([^@\s]+)@)?([^\s]+)$/, 2, 3);
+  // e.g. "run@http://localhost:8888/__jasmine__/jasmine.js:4320:27"
+  // or "http://localhost:8888/__jasmine__/jasmine.js:4320:27"
+  function parseFirefoxStyleFrames(lines) {
+    return tryParseFrames(lines, /^(([^@\s]+)@)?([^\s]+)$/, 2, 3);
   }
 
-  function tryExtractFrames(lines, regex, funcGroupIx, fileAndLineAndColGroupIx) {
+  function tryParseFrames(lines, regex, funcGroupIx, fileAndLineAndColGroupIx) {
     var converted = lines.map(function(line) {
       var overallMatch = line.match(regex);
       var fileLineColMatch;
